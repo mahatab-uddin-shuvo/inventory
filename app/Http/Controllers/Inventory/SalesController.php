4ef064e6877d6c93dd76a60ef76bafd0a5dab8de
Mahatab Uddin <mahatab.uddin@adndiginet.com>
2023-06-17 04:33:45 +0600

    validation check

--- app/Http/Controllers/Inventory/SalesController.php
+++ app/Http/Controllers/Inventory/SalesController.php
@@ -8,7 +8,7 @@ use App\Models\Product;
 use App\Models\Purchase;
 use App\Models\Sale;
 use Illuminate\Http\Request;
-
+use Validator;
 class SalesController extends BaseController
 {
     public function create(Request $request){
@@ -19,17 +19,32 @@ class SalesController extends BaseController
             $input = $request->all();
         }
 
+
         foreach ($input as $key =>$value){
             $sale =  new Sale();
-
-            $sale->product_id = $input[$key]['product_id'];
-            $sale->quantity = $input[$key]['quantity'];
-
-            $sale->save();
-
             $product =  Product::find($input[$key]['product_id']);
-            $product->stock_qty = $product->stock_qty - $input[$key]['quantity'];
-            $product->update();
+            if($product->stock_qty<1){
+                $data = [
+                    'name' => $product->name,
+                    'msg' => 'this product is not available'
+                ];
+                return $this->sendError($data, 'Logical issue');
+            }elseif ($product->stock_qty < $input[$key]['quantity']){
+                $data = [
+                    'name' => $product->name,
+                    'msg' => 'We do not have your total product quantity'
+                ];
+                return $this->sendError($data, 'Logical issue');
+            }
+            else{
+                $sale->product_id = $input[$key]['product_id'];
+                $sale->quantity = $input[$key]['quantity'];
+
+                $sale->save();
+
+                $product->stock_qty = $product->stock_qty - $input[$key]['quantity'];
+                $product->update();
+            }
         }
 
         return $this->sendResponse('', 'Sales created Successfully');
